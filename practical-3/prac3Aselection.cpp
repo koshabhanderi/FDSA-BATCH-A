e4c15fad089238cce1835834ccf0e10bf20d7c2f
koshabhanderi <koshabhanderi910@gmail.com>
2026-08-19 20:29:54 +0530

    Add files via upload

---
+++ practical-3/prac3Aselection.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks[] = {65, 32, 78, 45, 90, 21};
+    int n = 6;
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for(int j = i + 1; j < n; j++)
+        {
+            if(marks[j] < marks[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        int temp = marks[i];
+        marks[i] = marks[minIndex];
+        marks[minIndex] = temp;
+    }
+
+    cout << "Sorted marks using Selection Sort: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << marks[i] << " "<<endl;;
+    }
+
+    return 0;
+}
